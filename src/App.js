502babd382ce99b8ca141c40707205644adb2482
Thiago <thiagowirzdemoura@gmail.com>
2022-02-10 19:28:52 -0300

    remove: cart fixed values

--- src/App.js
+++ src/App.js
@@ -13,20 +13,7 @@ import CartPage from "./Cart";
 function App() {
   const lastUser = JSON.parse(localStorage.getItem("last-user"));
   const [user, setUser] = useState(lastUser);
-  const [cart, setCart] = useState([
-    {
-      name: "Coca-cola",
-      price: "4,50",
-      image:
-        "https://static.distribuidoracaue.com.br/media/catalog/product/cache/1/thumbnail/600x800/9df78eab33525d08d6e5fb8d27136e95/r/e/refrigerante-coca-cola-2-litros.jpg",
-    },
-    {
-      name: "pizza de calabresa",
-      price: "49,90",
-      image:
-        "https://t2.rg.ltmcdn.com/pt/posts/9/8/3/pizza_calabresa_e_mussarela_4389_600.jpg",
-    },
-  ]);
+  const [cart, setCart] = useState([]);
 
   return (
     <CartContext.Provider value={{ cart, setCart }}>
