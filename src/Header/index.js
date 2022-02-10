b046463d835d173abbed973ec8826ce2fc1ef130
Thiago <thiagowirzdemoura@gmail.com>
2022-02-10 13:19:49 -0300

    feat: logout

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,7 +1,26 @@
 import logo from "../assets/icone.jpg";
 import { User, Cart, Logo, ContainerRight, TopBar } from "./style";
+import UserContext from "../contexts/userContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleLogOut() {
+    if (!user) {
+      navigate("/sign-in");
+    } else {
+      if (
+        window.confirm("Você já está logado, gostaria de sair deste usuário?")
+      ) {
+        localStorage.setItem("last-user", null);
+        setUser(null);
+        navigate("/sign-in");
+      }
+    }
+  }
   return (
     <TopBar>
       <Logo to="/">
@@ -11,7 +30,7 @@ function Header() {
         <Cart to="/cart">
           <ion-icon name="cart"></ion-icon>
         </Cart>
-        <User to="sign-in">
+        <User onClick={handleLogOut}>
           <ion-icon name="person-sharp"></ion-icon>
         </User>
       </ContainerRight>
--- src/Header/style.js
+++ src/Header/style.js
@@ -1,56 +1,46 @@
-import styled from "styled-components"
-import { Link } from 'react-router-dom'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const TopBar = styled.div`
-    width: 100%;
-    height: 60px;
-    background-color: #006491 ;
+  width: 100%;
+  height: 60px;
+  background-color: #006491;
 
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    position: fixed;
-    top: 0
-
-`
+  position: fixed;
+  top: 0;
+`;
 
 const ContainerRight = styled.div`
-    width: 100px;
-    display: flex;
-    gap: 20px
-
-`
+  width: 100px;
+  display: flex;
+  gap: 20px;
+`;
 const Logo = styled(Link)`
-    margin-left: 20px;
-    & img{
-        width: 40px;
-        height: 40px;
+  margin-left: 20px;
+  & img {
+    width: 40px;
+    height: 40px;
 
-        border-radius: 60px;
-    }
-
-`
+    border-radius: 60px;
+  }
+`;
 
 const Cart = styled(Link)`
-    & ion-icon{
-        color: gray;
-        font-size: 30px;
-    }
+  & ion-icon {
+    color: gray;
+    font-size: 30px;
+  }
+`;
+const User = styled.button`
+  all: unset;
+  & ion-icon {
+    color: gray;
+    font-size: 30px;
+  }
+`;
 
-`
-const User = styled(Link)`
-    & ion-icon{
-        color: gray;
-        font-size: 30px;
-    }
-
-`
-
-export {
-    User,
-    Cart,
-    Logo,
-    ContainerRight,
-    TopBar
-}
+export { User, Cart, Logo, ContainerRight, TopBar };
